6756b91aac49a0a6f77fbc3b2dd8e5bc9f7217a5
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-11-20 00:01:20 +0530

    Added error page component

---
+++ src/error.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1 className="display-1">404 Not Found !!!</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Error;
